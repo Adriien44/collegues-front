bf0b330483cf38888138fc3b5fdeba1381700a84
Adrien CHAUVIN <adrienchauvin44@gmail.com>
2019-07-17 16:03:42 +0200

    correction environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
 
     // ajout d'une URL backend en mode développement
-  backendUrl: 'https://chauvin-collegues-api.herokuapp.com/'
+  backendUrl: 'https://chauvin-collegues-api.herokuapp.com/collegues'
 };
