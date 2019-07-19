8695ccca26bdfa699d629418ba7f39803b8b4cb9
Adrien CHAUVIN <adrienchauvin44@gmail.com>
2019-07-19 17:00:50 +0200

    Ajout gestion erreur serveur sur gallerie

--- src/app/gallerie/gallerie.component.ts
+++ src/app/gallerie/gallerie.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
 import { GallerieCollegue } from '../models/GallerieCollegue';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-gallerie',
@@ -9,13 +10,22 @@ import { GallerieCollegue } from '../models/GallerieCollegue';
 })
 export class GallerieComponent implements OnInit {
 
-  constructor(private _gallerie:DataService) { }
+  constructor(private _gallerie: DataService) { }
 
-  listePhoto:GallerieCollegue[];
-
+  listePhoto: GallerieCollegue[];
+  erreurListe: boolean = false;
+  erreur:boolean = false; 
+  msgError:string; 
 
   ngOnInit() {
-    return this._gallerie.getPhoto().subscribe(listeCollegue => this.listePhoto = listeCollegue);
+    return this._gallerie.getPhoto().subscribe(
+      listeCollegue => { if (listeCollegue.length == 0) {
+        this.erreurListe = true;
+      } else { 
+        (this.listePhoto = listeCollegue, this.erreurListe = false); 
+      }
+    }, 
+    (respError: HttpErrorResponse) => { this.erreur = true, this.msgError = "Oups... Serveur déconnecté"}
+    );
   }
-
 }
